8d26c14b0e701274a8c4d3b36f25d3a12b2ba608
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-02-17 10:49:18 +0300

    Remove core-publish

--- packages/core-publish/src/components/Typography/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Typography() {
-  return (
-    <div>This is the  Typography</div>
-  )
-}
--- packages/core-publish/src/index.js
+++
@@ -1,6 +0,0 @@
-import MuiButtonExample from './components/MuiButtonExample'
-import Typography from './components/Typography'
-import Footer from './components/Footer'
-import Page from './components/Page'
-
-export { MuiButtonExample, Typography, Page, Footer }
